e3386461fe235c95f04bea7ba5bf074b4943aab1
Fevzi Sühan Şahin <fevzisuhansahin@gmail.com>
2026-02-26 18:38:03 +0300

    Price was changed as multiciple fiftys (1550,1600)

--- server.js
+++ server.js
@@ -26,20 +26,20 @@ let kayanYaziMetni = "Gediz Sarraflar Derneği - Güncel Altın Fiyatları Ekran
 // Artık sadece Has Altın (ALTIN) fiyatını alıp bu standart katsayılarla çarpıyoruz.
 let ziynetUrunleri = [
 
-    { isim: "ÇEYREK", kod: "CEYREK", yeniAlis: 1.63, yeniSatis: 1.65, eskiAlis: 1.625, eskiSatis: 1.645, alisAyar: 0, satisAyar: 50 },
-    { isim: "YARIM", kod: "YARIM", yeniAlis: 3.27, yeniSatis: 3.29, eskiAlis: 3.24, eskiSatis: 3.275, alisAyar: 0, satisAyar: 50 },
-    { isim: "TAM", kod: "TAM", yeniAlis: 6.51, yeniSatis: 6.54, eskiAlis: 6.47, eskiSatis: 6.525, alisAyar: 0, satisAyar: 50 },
-    { isim: "GREMSE", kod: "GREMSE", yeniAlis: 16.22, yeniSatis: 16.30, eskiAlis: 16.14, eskiSatis: 16.26, alisAyar: 0, satisAyar: 50 },
-    { isim: "ATA", kod: "ATA", yeniAlis: 6.67, yeniSatis: 6.715, eskiAlis: 6.67, eskiSatis: 6.715, alisAyar: 0, satisAyar: 50 }
+    { isim: "ÇEYREK", kod: "CEYREK", yeniAlis: 1.63, yeniSatis: 1.65, eskiAlis: 1.625, eskiSatis: 1.645, alisAyar: 0, satisAyar: 0 },
+    { isim: "YARIM", kod: "YARIM", yeniAlis: 3.27, yeniSatis: 3.29, eskiAlis: 3.24, eskiSatis: 3.275, alisAyar: 0, satisAyar: 0 },
+    { isim: "TAM", kod: "TAM", yeniAlis: 6.51, yeniSatis: 6.54, eskiAlis: 6.47, eskiSatis: 6.525, alisAyar: 0, satisAyar: 0 },
+    { isim: "GREMSE", kod: "GREMSE", yeniAlis: 16.22, yeniSatis: 16.30, eskiAlis: 16.14, eskiSatis: 16.26, alisAyar: 0, satisAyar: 0 },
+    { isim: "ATA", kod: "ATA", yeniAlis: 6.67, yeniSatis: 6.715, eskiAlis: 6.67, eskiSatis: 6.715, alisAyar: 0, satisAyar: 0 }
 ];
 
 let gramUrunleri = [
-    { isim: "1 GR", kod: "KULCEALTIN", alisAyar: 0, satisAyar: 50 }
+    { isim: "1 GR", kod: "KULCEALTIN", alisAyar: 0, satisAyar: 0 }
 ];
 
 let piyasaUrunleri = [
-    { isim: "ALTIN", kod: "ALTIN", alisAyar: 0, satisAyar: 50 },
-    { isim: "GÜMÜŞ", kod: "GUMUSTRY", alisAyar: 0, satisAyar: 50 }
+    { isim: "ALTIN", kod: "ALTIN", alisAyar: 0, satisAyar: 0 },
+    { isim: "GÜMÜŞ", kod: "GUMUSTRY", alisAyar: 0, satisAyar: 0 }
 ];
 
 // --- YENİ: HAFIZAYI (JSON) YÜKLEME FONKSİYONU ---
@@ -111,10 +111,10 @@ function fiyatlariHesaplaVeYayinla(yeniAnaVeri) {
             isim: ayar.isim,
             saat: altinSaat,
             // Alışları aşağı (floor) yuvarla, Satışlara kar ekle ve yukarı (ceil) yuvarla
-            yeniAlis: Math.floor(hasAltinAlis * ayar.yeniAlis) + ayar.alisAyar,
-            yeniSatis: Math.ceil(hasAltinSatis * ayar.yeniSatis) + ayar.satisAyar,
-            eskiAlis: Math.floor(hasAltinAlis * ayar.eskiAlis) + ayar.alisAyar,
-            eskiSatis: Math.ceil(hasAltinSatis * ayar.eskiSatis) + ayar.satisAyar
+            yeniAlis: Math.floor((hasAltinAlis * ayar.yeniAlis) / 50) * 50 + ayar.alisAyar,
+            yeniSatis: Math.ceil((hasAltinSatis * ayar.yeniSatis) / 50) * 50 + ayar.satisAyar,
+            eskiAlis: Math.floor((hasAltinAlis * ayar.eskiAlis) / 50) * 50 + ayar.alisAyar,
+            eskiSatis: Math.ceil((hasAltinSatis * ayar.eskiSatis) / 50) * 50 + ayar.satisAyar
         });
     });
 
@@ -124,8 +124,8 @@ function fiyatlariHesaplaVeYayinla(yeniAnaVeri) {
             tvVerisi["Gram Altın"].push({
                 isim: ayar.isim,
                 saat: saatAyirla(anaVeri[ayar.kod].tarih),
-                alis: Math.floor(anaVeri[ayar.kod].alis) + ayar.alisAyar,
-                satis: Math.ceil(anaVeri[ayar.kod].satis) + ayar.satisAyar
+                alis: Math.floor((anaVeri[ayar.kod].alis) / 50) * 50 + ayar.alisAyar,
+                satis: Math.ceil((anaVeri[ayar.kod].satis) / 50) * 50 + ayar.satisAyar
             });
         }
     });
@@ -136,8 +136,8 @@ function fiyatlariHesaplaVeYayinla(yeniAnaVeri) {
             tvVerisi["Piyasalar"].push({
                 isim: ayar.isim,
                 saat: saatAyirla(anaVeri[ayar.kod].tarih),
-                alis: Math.floor(anaVeri[ayar.kod].alis) + ayar.alisAyar,
-                satis: Math.ceil(anaVeri[ayar.kod].satis) + ayar.satisAyar
+                alis: Math.floor((anaVeri[ayar.kod].alis) / 50) * 50 + ayar.alisAyar,
+                satis: Math.ceil((anaVeri[ayar.kod].satis) / 50) * 50 + ayar.satisAyar
             });
         }
     });
